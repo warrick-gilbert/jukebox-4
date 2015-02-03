e5ac5dcc87dad541cf7d908ee1c0075c78e91ad2
Warrick Gilbert <warrick_gilbert@yahoo.co.nz>
2015-02-03 09:01:24 -0800

    Finished Part 3

---
+++ app.js
@@ -0,0 +1,108 @@
+var LIBRARY = [
+  {title: 'C Major Scale', notes: 'A B C D E F G' },
+  {title: 'Chromatic Scale', notes: 'A A# B C C# D D# E F F# G G#' },
+  {title: 'Random Song', notes: 'A B*2 C D A*4 D E*2 F A B A A*2' },
+  {title: 'Adup Licate', notes: 'A B*2 C D A*4 D E*2 F A B A A*2' },
+  {title: 'Yankee Doodle', notes: 'C F*4 C F*4 B C D A*2 B*2 A B*2 C' },
+  {title: 'Descending Notes', notes: 'G F E D C B A G F E D C B A' }
+];
+
+var BPM = 600;
+
+
+// Add a song with the given title and notes to the library.
+var addSongToLibrary = function(title, notes) {
+  $('#library-list').append("<li class='noselect'>" +
+                                "<i class='fa fa-bars'></i>" +
+                                "<i class='fa fa-trash'></i>" +
+                                "<span class='title'>" + title + "</span>" +
+                                "<div class='notes'>" + notes + "</div>" +
+                              "</li>");
+};
+
+
+// Add all LIBRARY songs to the library.
+var initializeLibrary = function() {
+  for(var i=0; i < LIBRARY.length; i+=1) {
+    addSongToLibrary(LIBRARY[i].title, LIBRARY[i].notes);
+  }
+};
+
+
+// Play all songs in the playlist.
+var playAll = function() {
+
+  // Grab the top song in the queue, parse its notes and play them.
+  // Then recurse until there are no more songs left in the queue.
+  //
+  var playNext = function() {
+    var songItem = $('#playlist-list li:first-child');
+
+    if (songItem.length == 0) {
+      // No more songs.
+
+      // Re-enable the play button.
+      $('#play-button').attr('disabled', false).text('Play All');
+
+      // Fade out the message.
+      $('#message').fadeOut();
+      return;
+    }
+
+    var title = songItem.find('.title').text();
+    var notes = songItem.find('.notes').text();
+    var song = parseSong(notes);
+
+    $('#message').html("Now playing: <strong>" + title + "</strong>").show();
+
+    playSong(song, BPM, function() {
+      songItem.remove();
+      $('#library-list').append(songItem);
+      playNext();
+    });
+  };
+
+  // Disable the play button to start.
+  $('#play-button').attr('disabled', true).text('Playing');
+
+  playNext();
+}
+
+
+$(document).ready(function() {
+
+  // Initialize the library with some songs.
+  initializeLibrary();
+
+  // Play all songs in the playlist when the "play" button is clicked.
+  $('#play-button').on('click', playAll);
+
+  // click on trash icon removes song
+  $("li .fa-trash").on("click", function(){
+    $(this).parent().slideUp(500, function(){
+      $(this).remove(); // the "this" here is the "this.parent" from above
+    });  
+  });
+
+  //  dbl click on song title slideDown to reveal notes
+  $("ul").on("dblclick", "li", function(){
+    $(this).find(".notes").slideToggle(300);
+  });
+
+  // message fade in and then out
+  $("#message").fadeIn(800, function(){
+    setTimeout(function() { $("#message").fadeOut(800); }, 3000);
+  });
+
+  $( "#playlist-list, #library-list" ).sortable({connectWith: ".connectedSortable"});
+  $( "#playlist-list, #library-list" ).disableSelection();
+
+  // filter the library
+  $('#filter-library').on('keyup', function () {
+    var text1 = $(this).val();
+    $( "li" ).hide();
+    $( "li" ).filter(':contains(' + text1 + ')').show();
+  });
+
+
+});
